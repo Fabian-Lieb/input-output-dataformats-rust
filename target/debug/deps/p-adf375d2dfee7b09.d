964d1d9f549acf5e772c3919efd571390b132359
Fabian Lieb <fabian.lieb@peptech.de>
2023-05-01 11:41:11 +0000

    init project

---
+++ target/debug/deps/p-adf375d2dfee7b09.d
@@ -0,0 +1,5 @@
+/home/user01/programmieren/projects/input-output-dataformats-rust/target/debug/deps/p-adf375d2dfee7b09: src/main.rs
+
+/home/user01/programmieren/projects/input-output-dataformats-rust/target/debug/deps/p-adf375d2dfee7b09.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/p.d
@@ -0,0 +1,1 @@
+/home/user01/programmieren/projects/input-output-dataformats-rust/target/debug/p: /home/user01/programmieren/projects/input-output-dataformats-rust/src/main.rs
